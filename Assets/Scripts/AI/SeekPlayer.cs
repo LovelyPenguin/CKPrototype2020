4450a58812241ff5c6040153ed0f9c242f3c860a
lyp3238 <lyp3238@gmail.com>
2020-09-11 16:42:21 +0900

    AI Prototype 작성

---
+++ Assets/Scripts/AI/SeekPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SeekPlayer : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+    public Transform player;
+    [Range(1,180)]
+    public float detectAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CalculateAngle(player.position));
+    }
+
+    private float CalculateAngle(Vector3 target)
+    {
+        Vector3 v = target - transform.position;
+        float value = Mathf.Acos(
+            Vector3.Dot(transform.forward, Vector3.Normalize(target - transform.position)
+            )) * Mathf.Rad2Deg;
+        float originAngle = Mathf.Atan2(v.x, v.z) * Mathf.Rad2Deg;
+        return value;
+    }
+}
