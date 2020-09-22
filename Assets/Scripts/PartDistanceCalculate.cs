bcc0643c6ebd02f6de18281ef368f2789e5c05bb
lyp3238 <lyp3238@gmail.com>
2020-09-22 09:10:50 +0900

    흡혈 방식 프로토타입

---
+++ Assets/Scripts/PartDistanceCalculate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartDistanceCalculate : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float slide;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += transform.up;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            player.transform.parent = null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("PLAYER");
+            //Vector3 temp = player.transform.position;
+            //player.transform.parent = gameObject.transform;
+            //player.transform.position = temp;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 tra = transform.localPosition;
+        //tra.y = player.transform.position.y;
+        Gizmos.DrawSphere(tra * 1.3f, 0.1f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + transform.up * 0.5f, 0.1f);
+        Gizmos.DrawSphere(transform.position - transform.up * 0.5f, 0.1f);
+    }
+}
---
+++ Assets/Scripts/ShootRaycast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootRaycast : MonoBehaviour
+{
+    [Range(0.01f, 5)]
+    public float distance;
+    [SerializeField]
+    private Transform pin;
+    public RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.DrawRay(pin.position, pin.forward * distance, Color.blue, 0.3f);
+            if (Physics.Raycast(pin.position, pin.forward, out hit, distance))
+            {
+                if (hit.transform.CompareTag("Finish"))
+                {
+                    transform.position = hit.point;
+                    transform.parent = hit.transform;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.4f);
+        Gizmos.DrawRay(pin.position, pin.forward * distance);
+    }
+}
