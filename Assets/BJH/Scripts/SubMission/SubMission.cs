bd30e9036721e2558a0140dd72aa931921df438c
897754 <42639194+897754@users.noreply.github.com>
2020-10-01 14:48:10 +0900

    서브미션 구현중

---
+++ Assets/BJH/Scripts/SubMission/SubMission.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName ="SubMission", menuName ="Scriptable/Missions")]
+public class SubMission : ScriptableObject
+{
+    public enum Kind
+    {
+        SuckTimes,
+        MakeNoiseSec,
+        SuckPart
+    }
+
+    //Inspector
+    public Kind missionKind;
+    public float targetArg;
+    //Inspector
+
+    [HideInInspector]
+    public float argf;
+    [HideInInspector]
+    public bool isCompleted = false;
+    [HideInInspector]
+    public bool isFinished = false;
+}
---
+++ Assets/BJH/Scripts/SubMission/SubMissionManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMissionManager : MonoBehaviour
+{
+    public static SubMissionManager instance;
+
+    public List<SubMission> missions;
+
+    private void Awake()
+    {
+        if (!instance) instance = this;
+    }
+
+    private void Update()
+    {
+        CheckMissions();
+    }
+
+    void CheckMissions()
+    {
+        SubMission mission;
+        for (int i = 0; i < missions.Count; i++)
+        {
+            mission = missions[i];
+
+            if (mission.isFinished) continue;
+
+            switch (mission.missionKind)
+            {
+                case SubMission.Kind.MakeNoiseSec:
+                    {
+                        CheckMakeNoiseSec(mission);
+                    }
+                    break;
+                case SubMission.Kind.SuckTimes:
+                    {
+                        CheckSuckTimes(mission);
+                    }
+                    break;
+                case SubMission.Kind.SuckPart:
+                    {
+                        CheckSuckPart(mission);
+                    }
+                    break;
+
+                default:
+                    {
+                        missions.Remove(mission);
+                    }
+                    break;
+
+            }
+        }
+    }
+
+    void CheckMakeNoiseSec(SubMission mission)
+    {
+        //if(isMakingNose)
+        mission.argf += Time.deltaTime;
+        if (mission.targetArg <= mission.argf)
+        {
+            mission.isCompleted = true;
+            mission.isFinished = true;
+        }
+    }
+    void CheckSuckTimes(SubMission mission)
+    {
+        //if(Suck())
+        mission.argf += 1;
+
+        if(mission.targetArg <= mission.argf)
+        {
+            mission.isCompleted = true;
+            mission.isFinished = true;
+        }
+    }
+    void CheckSuckPart(SubMission mission)
+    {
+        //if(sucked.part == mission.part)
+        //{
+        //    mission.isCompleted = true;
+        //    mission.isFinished = true;
+        //}
+    }
+}
