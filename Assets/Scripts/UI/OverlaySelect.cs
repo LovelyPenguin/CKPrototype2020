362e6aaf8b705377e0d92f1c48f110e1de4fba5e
lyp3238 <lyp3238@gmail.com>
2020-10-08 20:26:56 +0900

    게임 클리어 및 실패 UI 제작

---
+++ Assets/Scripts/UI/OverlaySelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OverlaySelect : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject localSelectImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSettings.instance.GetComponent<PauseManager>().ToggleSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        localSelectImage.transform.position = gameObject.transform.position;
+    }
+}
