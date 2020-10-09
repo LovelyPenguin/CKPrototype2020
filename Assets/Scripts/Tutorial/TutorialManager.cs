e6431bb8f185a38c42b53895ccbd93e3d2c1ea5f
lyp3238 <lyp3238@gmail.com>
2020-10-09 21:47:05 +0900

    캐릭터 및 방 콜라이더 수정

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject movementTutorial;
+    public Text messageBox;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TutorialStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TutorialStart()
+    {
+        yield return new WaitForSeconds(2f);
+        Phase1();
+    }
+
+    public void Phase1()
+    {
+        movementTutorial.SetActive(true);
+    }
+    public void Phase2()
+    {
+        movementTutorial.SetActive(false);
+        messageBox.text = "인간을 찾기";
+    }
+
+    public void Phase3()
+    {
+        messageBox.text = "";
+    }
+}
---
+++ Assets/Scripts/UI/GetClearStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetClearStatus : MonoBehaviour
+{
+    public SubMissionManager subMissons;
+    private List<SubMission> clear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        SubMission misson;
+        int clearCount = 0;
+        clear = subMissons.GetResult();
+        for (int i = 0; i < clear.Count; i++)
+        {
+            misson = clear[i];
+            if (misson.isCompleted)
+            {
+                clearCount++;
+            }
+        }
+        Debug.Log(clearCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
