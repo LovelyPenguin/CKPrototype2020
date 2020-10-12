74beed14449ebf9398ff0b475414e3c1e13848fc
lyp3238 <lyp3238@gmail.com>
2020-10-12 22:18:54 +0900

    스테이지 선택화면 수정

---
+++ Assets/Scripts/UI/SetOpacity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetOpacity : MonoBehaviour
+{
+    public List<Text> texts;
+    public List<Image> images;
+
+    public float alpha = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < images.Count; i++)
+        {
+            images[i].color = new Color(1, 1, 1, alpha);
+        }
+
+        for (int i = 0; i < texts.Count; i++)
+        {
+            texts[i].color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
